44cc535d2828afc6d5e8471f9c62a7f952dbdad0
Yu Zhang <yuz9@dmserv5.cs.illinois.edu>
2022-10-29 02:12:46 -0500

    add tqdm

--- preprocess.py
+++ preprocess.py
@@ -11,6 +11,6 @@ json_name = args.json_name
 
 with open(f'{dataset}/{json_name}') as fin, open(f'{dataset}/dataset.txt', 'w') as fou1, open(f'{dataset}/labels.txt', 'w') as fou2:
 	for line in fin:
-		js = json.loads(line)
-		fou1.write(js['text'] + ' ' + js['user'] + ' ' + ' '.join(js['tags']) + ' ' + ' '.join(js['name']) + '\n')
-		fou2.write(js['labels'][-1] + '\n')
+		data = json.loads(line)
+		fou1.write(data['text'] + ' ' + data['user'] + ' ' + ' '.join(data['tags']) + ' ' + ' '.join(data['name']) + '\n')
+		fou2.write(data['labels'][-1] + '\n')
